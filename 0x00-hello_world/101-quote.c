58ae1b049abfb41af90524ceef089ea3f7013ba7
fuadhasen <fuadhas6634@gmail.com>
2023-06-15 16:18:52 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 
 /**
  *  main - Entry point
- * 
- *   Return: Always 1 (Error)
+ *
+ *  Return: Always 1 (failure - error message printed to stderr)
  */
 int main(void)
 {
-		const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-			ssize_t len = sizeof(msg) - 1;
-				ssize_t ret = write(STDERR_FILENO, msg, len);
+	    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	        ssize_t len = sizeof(msg) - 1;
+		    ssize_t ret = write(STDERR_FILENO, msg, len);
 
-					return (ret != len);
+		        return (ret != len);
 }
