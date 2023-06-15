e261565d00ab8a2b90c993a6991cef66a6974bba
fuadhasen <fuadhas6634@gmail.com>
2023-06-15 15:04:35 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo 'int main(void){puts("Programming is like building a multilingual puzzel");return 0}'
+int main() { puts("Programming is like building a multilingual puzzle"); return 0; }
