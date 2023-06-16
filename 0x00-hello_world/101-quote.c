db7918e6c1f32735eb560c8dfb4e26d727759965
fuadhasen <fuadhas6634@gmail.com>
2023-06-15 23:05:50 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
  *  main - Entry point
- *
- * Return: Always 1 (failure - error message printed to stderr)
+ *  Return:1
  */
 int main(void)
 {
-		const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-			ssize_t len = sizeof(msg) - 1;
-				ssize_t ret = write(STDERR_FILENO, msg, len);
-
-					return (ret != len);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
