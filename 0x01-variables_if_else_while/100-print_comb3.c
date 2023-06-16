bf754374fa4be7d51f4bacddcd90a19764de9120
fuadhasen <fuadhas6634@gmail.com>
2023-06-16 14:54:20 -0700

    f

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 9; ++i)
 	{
-		for (j = i +1; j <= 9; ++j)
+		for (j = i + 1; j <= 9; ++j)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
