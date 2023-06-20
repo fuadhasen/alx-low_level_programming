31a54256983522cae06b7daaeef63c738845a0d6
fuadhasen <fuadhas6634@gmail.com>
2023-06-20 04:59:30 -0700

    f

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
  * Return: 1 and prints + if n is greater than zero,
- * Return: 0 and prints 0 if n is zero,
- * Return: -1 and prints - if n is less than zero
+ *         0 and prints 0 if n is zero,
+ *        -1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
@@ -20,7 +20,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
