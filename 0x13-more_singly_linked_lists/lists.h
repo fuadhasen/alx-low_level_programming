c1a8bc9fd3e818d51acba011514fd64be9baeabe
fuadhasen <fuadhas6634@gmail.com>
2023-07-31 22:00:52 -0700

    printlist

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST
-#define LIST
+#ifndef LISTS_H
+#define LIST_H
 
 
 #include <stddef.h>
@@ -30,4 +30,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 #endif
