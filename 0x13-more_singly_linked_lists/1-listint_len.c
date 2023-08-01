a1096e1ea5c781bc46e29b668790e3b72c183da4
fuadhasen <fuadhas6634@gmail.com>
2023-07-31 23:09:28 -0700

    list len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include <lists.h>
+#include <stdio.h>
+
+/**
+ * listint_len - list length
+ *
+ * @h: head pointer to first node
+ *
+ * Return: return number of elements
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+	listint_t *temp;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
