d1dad3546699310a0cc2a02629da601e30b63e44
fuadhasen <fuadhas6634@gmail.com>
2023-08-01 06:37:25 -0700

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint - free all the list by traversing
+ * @head: pointer to the first node
+ *
+ * Return: no return
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp, *nxt;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		nxt = temp->next;
+		free(temp);
+		temp = nxt;
+	}
+}
