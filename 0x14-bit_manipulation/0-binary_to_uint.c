ad1c23313f3ff8df68d7a15b261c13233901d765
fuadhasen <fuadhas6634@gmail.com>
2023-08-03 04:41:19 -0700

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,9 +24,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		j <<= 1;
+		res <<= 1;
 		if (b[i] == '1')
-			j += 1;
+			res += 1;
 	}
-	return (j);
+	return (res);
 }
