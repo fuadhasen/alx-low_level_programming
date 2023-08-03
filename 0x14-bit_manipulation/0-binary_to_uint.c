f33859c904f01b02b3a9262a90998841dcc168c4
fuadhasen <fuadhas6634@gmail.com>
2023-08-02 23:21:52 -0700

    binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binay string to unsigned int
+ * @b: pointer to first elemnt in the string
+ *
+ * Return: res, if not return 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int bit;
+	size_t n = 0, i;
+	unsigned int res = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[n] !='\0')
+		n++;
+
+	for (i = 0; i < n; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			bit = b[i] - '0';
+			res = (res << 1) | bit;
+		}
+		else
+
+			return (0);
+
+	}
+	return (res);
+}
+
+
+
+
