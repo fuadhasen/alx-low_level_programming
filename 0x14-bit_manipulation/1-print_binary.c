6b79eb863f7f92f6f4fd5ef47add010ba75ba895
fuadhasen <fuadhas6634@gmail.com>
2023-08-03 05:16:07 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,15 +11,24 @@ void print_binary(unsigned long int n)
 {
 	int size = sizeof(unsigned long int) * 8;
 	int shift = size - 1, i;
+	int flag = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		if ((n >> shift) & 1)
+		{
 			_putchar('1');
+			flag = 1;
+		}
 		else
-			_putchar('0');
+		{
+			if (flag)
+				_putchar('0');
+		}
 
 		shift--;
 	}
+	if (!flag)
+		_putchar('0');
 }
 
