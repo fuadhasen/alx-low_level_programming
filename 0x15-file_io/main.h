1cb64119b212b7f47ed56d8563df92d31dfd0c88
fuadhasen <fuadhas6634@gmail.com>
2023-08-07 13:12:06 -0700

    read

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,13 @@
-#ifndef main
-#define main
+#ifndef MAIN
+#define MAIN
 
-#include <sys/stat.h>
-#include <fctnl.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
