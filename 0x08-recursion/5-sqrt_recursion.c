dd6fa882347ff8a25f9be6725d60287e5a804756
fuadhasen <fuadhas6634@gmail.com>
2023-08-30 12:06:04 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * check - check
+ * @a: guess num
+ * @b: the number
+ *
+ * Return: sqrt
+ */
+
+int check(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (a + 1, b);
+}
+
+/**
+ * _sqrt_recursion - root
+ * @n: number
+ *
+ * Return: the sqrt
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (check(1, n));
+}
