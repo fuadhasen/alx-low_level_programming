0704b51addac9e400a10badc10c9a38a4feb4489
fuadhasen <fuadhas6634@gmail.com>
2023-09-02 00:41:53 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @c: number of argument
+ * @av: argument
+ *
+ * Return: 0
+ */
+
+int main(int ac, char *av[])
+{
+	printf("%d\n", ac);
+	return (0);
+}
