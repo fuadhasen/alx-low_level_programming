f43a749a99ced2d7a44de86f998523b98004b746
fuadhasen <fuadhas6634@gmail.com>
2023-09-02 00:37:32 -0700

    ac

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,17 +5,14 @@
  * @ac: number of argument
  * @av: argumnet
  *
- * Return: 0 on success
+ * Return: 0 on succesis
  */
 
 int main(int ac, char *av[])
 {
-	int i;
-
-	for (i = 0; i < ac; i++)
+	if (ac > 0)
 	{
-		printf("%s ", av[i]);
+		printf("%s\n", av[0]);
 	}
-	printf("\n");
 	return (0);
 }
