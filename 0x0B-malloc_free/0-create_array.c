f593f7a21d3868476350200de517db03d7dbb778
fuadhasen <fuadhas6634@gmail.com>
2023-09-05 03:36:00 -0700

    f

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	str = malloc(sizeof(char) * (size + 1));
+	str = (char *) malloc(sizeof(char) * (size + 1));
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
