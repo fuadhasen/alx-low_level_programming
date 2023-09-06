a7132c355b29c6dfc1bdd60ed1afd9c14852f586
fuadhasen <fuadhas6634@gmail.com>
2023-09-06 00:36:38 -0700

    pu no

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,10 @@ char *str_concat(char *s1, char *s2)
 	size_t len1 = 0, len2 = 0;
 
 	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	{
+		newstr[0] = '\0';
+		return (newstr);
+	}
 	if (s1 != NULL)
 		len1 = strlen(s1);
 	if (s2 != NULL)
