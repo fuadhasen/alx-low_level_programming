c605e85ddd9a8258d21272a3431333e7ddab7fd7
fuadhasen <fuadhas6634@gmail.com>
2023-09-12 02:32:35 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - structure name dog
+ * @name: name
+ * @age: age
+ * @owner: owe
+ *
+ * Description: structure data type with name dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /*DOG*/
