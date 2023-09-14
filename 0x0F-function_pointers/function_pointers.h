1c5998150261bab3e59aa5a53abf0183cc886097
fuadhasen <fuadhas6634@gmail.com>
2023-09-14 02:31:06 -0700

    f

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,7 @@
-#include <stddef.h>
 #ifndef H
 #define H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
