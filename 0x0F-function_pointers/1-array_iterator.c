8ebd7821e9dfb0736ccce8a431cbeecb4c58f56a
fuadhasen <fuadhas6634@gmail.com>
2023-09-14 02:57:33 -0700

    f

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,7 +17,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (action != NULL)
 	{
 		for (i = 0; i < size; i++)
-			action(array[i]);
+		{
+			if (array != NULL)
+				action(array[i]);
+		}
 	}
 }
 
