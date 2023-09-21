39867f1021de5851b04d8bf4cd2a4cb434cee195
fuadhasen <fuadhas6634@gmail.com>
2023-09-20 23:32:15 -0700

    f

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_list - print the list
+ * @h: head pointer
+ *
+ *
+ * Return: num of nod
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *new1;
+	int count = 0;
+
+	new1 = h;
+	while (new1 != NULL)
+	{
+		if (new1->str != NULL)
+		{
+			printf("[%d] %s\n", new1->len, new1->str);
+			count++;
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+			count++;
+		}
+		new1 = new1->next;
+
+	}
+	return (count);
+}
+
+
+
