fb4f3b19087eb423107a9f4b396b5ec11255b933
fuadhasen <fuadhas6634@gmail.com>
2023-10-05 13:17:07 -0700

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (access(filename, F_OK) == -1)
 	{
-		fd = open(filename, fileperm);
+		fd = open(filename, O_CREAT | O_WRONLY, fileperm);
 		if (fd == -1)
 			return (-1);
 		if (text_content != NULL)
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		fd = open(filename, O_WRONLY |  O_TRUNC | O_APPEND, fileperm);
+		fd = open(filename, O_WRONLY | O_APPEND, fileperm);
 		if (fd == -1)
 			return (-1);
 		if (text_content != NULL)
