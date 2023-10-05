a3e3e7e47c2c45548a4c6fdac6dc0c976abc5c60
fuadhasen <fuadhas6634@gmail.com>
2023-10-05 13:05:59 -0700

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,6 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	mode_t fileperm = S_IRUSR | S_IWUSR;
 
-	if (filename == NULL)
-		return (-1);
-
 	if (access(filename, F_OK) == -1)
 	{
 		fd = open(filename, fileperm);
@@ -24,6 +21,7 @@ int create_file(const char *filename, char *text_content)
 		if (text_content != NULL)
 		{
 			ssize_t byte = write(fd, text_content, strlen(text_content));
+
 			if (byte == -1)
 				return (-1);
 		}
@@ -37,10 +35,11 @@ int create_file(const char *filename, char *text_content)
 		if (text_content != NULL)
 		{
 			ssize_t byte = write(fd, text_content, strlen(text_content));
+
 			if (byte == -1)
 				return (-1);
 		}
 	}
-	close (fd);
+	close(fd);
 	return (1);
 }
