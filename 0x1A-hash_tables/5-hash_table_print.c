c7842dae35da41293d43a20f51ad86c495ac8c6f
fuadhasen <fuadhas6634@gmail.com>
2024-02-18 11:34:12 -0800

    ht-print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,15 +9,22 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *temp;
+	int comma = 0;
+
+	if (ht == NULL)
+		return;
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		temp = ht->array[i];
-		while (temp)
+		while (temp != NULL)
 		{
+			if (comma)
+				printf(", ");
 			printf("'%s': '%s'", temp->key, temp->value);
 			temp = temp->next;
+			comma = 1;
 		}
 	}
 	printf("}\n");
