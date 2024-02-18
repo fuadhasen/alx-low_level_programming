97242aede2d929215355f597629bd8e3c35f0876
fuadhasen <fuadhas6634@gmail.com>
2024-02-18 06:01:54 -0800

    ht-search

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,36 +1,35 @@
 #include "hash_tables.h"
 /**
- * hash_table_set - set the node on the hash table
+ * hash_table_get - set the node on the hash table
  * @key: key
- * @value: value to be stored on the Ht
  * @ht: pointer to Ht
  *
- * Return: 1 success, 0 Fail
+ * Return: return value or NULL
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-        hash_node_t *head, *temp;
-        unsigned long int idx;
+	hash_node_t *head, *temp;
+	unsigned long int idx;
 
-        if (key == NULL || ht == NULL)
-                return (NULL);
+	if (key == NULL || ht == NULL)
+		return (NULL);
 
 	idx = key_index((const unsigned char *)key, ht->size);
-        head = ht->array[idx];
-        if (head == NULL)
-        {
+	head = ht->array[idx];
+	if (head == NULL)
+	{
 		return (NULL);
-        }
-        else
-        {
-                if (strcmp(head->key, key) == 0)
-                {
-                        return (head->value);
+	}
+	else
+	{
+		if (strcmp(head->key, key) == 0)
+		{
+			return (head->value);
 		}
-                else
+		else
 		{
 			temp = head;
-			while(temp)
+			while (temp)
 			{
 				if (strcmp(temp->key, key) == 0)
 					return (temp->value);
